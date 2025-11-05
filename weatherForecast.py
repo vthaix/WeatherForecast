346426d332f9e93e8dd403514296e8e03f557cb0
vthaix <cheat3049@gmail.com>
2025-11-06 01:17:51 +0700

    Update template directory path for Flask app
    tmp

--- weatherForecast.py
+++ weatherForecast.py
@@ -5,7 +5,7 @@ import asyncio
 import os
    # Cho phép fetch từ file HTML
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, "controllers")
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR)
 CORS(app)
