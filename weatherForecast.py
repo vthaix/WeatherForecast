53d1570cc432bf8d04bfcc1a87447a2a07edd60b
vthaix <cheat3049@gmail.com>
2025-11-06 01:24:44 +0700

    Refactor Flask app initialization for template folder
    finished

--- weatherForecast.py
+++ weatherForecast.py
@@ -3,11 +3,10 @@ from flask_cors import CORS
 import python_weather
 import asyncio
 import os
-   # Cho phép fetch từ file HTML
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-
-app = Flask(__name__, template_folder=TEMPLATE_DIR)
+app = Flask(
+    __name__,
+    template_folder=os.path.join(os.path.dirname(__file__), "templates")
+)
 CORS(app)
 @app.get("/")
 def home():
